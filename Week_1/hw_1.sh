95bc10a9323240b8ba1770ac85b258002e81c0c3
llai7 <llai7@uci.edu>
2023-01-12 13:27:00 -0800

    First Try

---
+++ Week_1/hw_1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --job-name=test    ## Name of the job.
+#SBATCH -A ecoevo283       ## account to charge
+#SBATCH -p standard        ## partition/queue name
+#SBATCH --cpus-per-task=1  ## number of cores the job needs
+#SBATCH -array=1-12        ## discussed more below
+
+
